9680c3d3c9329dff4f46864b01fd25b53be8b753
theOlanrewaju <olanrewajulanzy@gmail.com>
2024-08-29 04:49:26 +0100

    hero blog and  footer

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const hamburger = document.querySelector(".hamburger");
-  const nav = document.querySelector(".nav");
+// document.addEventListener("DOMContentLoaded", function () {
+//   const hamburger = document.querySelector(".hamburger");
+//   const nav = document.querySelector(".nav");
 
-  hamburger.addEventListener("click", function () {
-    hamburger.classList.toggle("active");
-    nav.classList.toggle("active");
-    console.log("Toggle clicked");
-  });
-});
+//   hamburger.addEventListener("click", function () {
+//     hamburger.classList.toggle("active");
+//     nav.classList.toggle("active");
+//     console.log("Toggle clicked");
+//   });
+// });
